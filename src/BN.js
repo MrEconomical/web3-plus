46067d698768dcf53c01c4a16ac0d1c394191a4e
MrEconomical <47700125+MrEconomical@users.noreply.github.com>
2022-03-26 16:30:34 -0700

    fix padding

--- src/BN.js
+++ src/BN.js
@@ -1,15 +1,21 @@
 const Web3Utils = require("web3-utils")
 const BN = Web3Utils.BN
 
+// Assert error
+
 function assert(val, msg) {
     if (!val) throw new Error(msg || "Assertion failed")
 }
 
+// Utilities
+
 BN.prototype.toParsed = function(decimals = 18) {
-    const padded = this.toString(10, decimals + 1)
+    const padded = this.toString().padStart(decimals + 1, "0")
     const parsed = `${padded.slice(0, -decimals)}.${padded.slice(-decimals)}`.replace(/0+$/g, "")
     return parsed.endsWith(".") ? parsed.slice(0, -1) : parsed
 }
+
+// Arithmetics
 
 BN.prototype.pown = function(num) {
     assert(typeof num === "number")
