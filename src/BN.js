6f926b2a9045aabf3287ba991c6056baaa0fb25d
MrEconomical <47700125+MrEconomical@users.noreply.github.com>
2022-03-26 16:40:29 -0700

    add format and unparse

--- src/BN.js
+++ src/BN.js
@@ -15,6 +15,22 @@ BN.prototype.toParsed = function(decimals = 18) {
     return parsed.endsWith(".") ? parsed.slice(0, -1) : parsed
 }
 
+BN.prototype.f = function(decimals = 18, precision = 2) {
+    return (+this.toParsed(decimals)).toLocaleString(undefined, {
+        minimumFractionDigits: precision,
+        maximumFractionDigits: precision
+    })
+}
+
+BN.unparse = function(num, decimals = 18) {
+    const number = num.toString()
+    if (number.includes(".")) {
+        const parts = number.split(".")
+        return new BN(parts[0] + parts[1].slice(0, decimals) + "0".repeat(Math.max(decimals - parts[1].length, 0)))
+    }
+    return new BN(number + "0".repeat(decimals))
+}
+
 // Arithmetics
 
 BN.prototype.pown = function(num) {
