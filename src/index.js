6bdbcc283a882c394d9c5026eeb7d8f2e8d5300c
MrEconomical <47700125+MrEconomical@users.noreply.github.com>
2022-03-26 17:17:16 -0700

    add class

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
+// Web3 modules
+
 const Web3 = require("web3")
+const Account = require("./account.js")
 const { _BN, BN } = require("./BN.js")
+
+// Web3 plus class
+
+class Web3Plus extends Web3 {
+    
+}
+
+// Export BN and class
 
 module.exports = {
     _BN,
     BN,
-    Web3
+    Web3Plus
 }
