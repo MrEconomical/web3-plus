51e46e87633bfe55af0c0e81e82e949b2d489277
MrEconomical <47700125+MrEconomical@users.noreply.github.com>
2022-03-26 17:13:10 -0700

    add account methods

--- src/account.js
+++ src/account.js
@@ -1,7 +1,31 @@
 // Account utils
 
 class Account {
+    // Copy Web3 account properties to instance
 
+    constructor(web3, account) {
+        this.web3 = web3
+        for (const property in account) {
+            this[property] = account[property]
+        }
+        this.nonce = 0
+    }
+
+    // Sign transaction from account
+
+    async signTx(params) {
+        return await this.signTransaction({
+            from: this.address,
+            nonce: account.nonce,
+            ...params
+        })
+    }
+
+    // Update account nonce
+
+    async updateNonce() {
+        this.nonce = await web3.eth.getTransactionCount(this.address)
+    }
 }
 
 module.exports = Account
